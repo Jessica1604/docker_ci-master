c7ed60dadf4bfb71bcc56651dfa0a70c1c7ce385
Jessica1604 <1538193262@qq.com>
2019-12-16 17:34:24 +0800

    updte

--- webhooks.js
+++ webhooks.js
@@ -39,11 +39,11 @@ handler.on('push', function (event) {
         event.payload.repository.name,
         event.payload.ref);
         // 分支判断
-        if(event.payload.ref === 'refs/heads/master'){
-            console.log('deploy master..')
-            run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
+        // if(event.payload.ref === 'refs/heads/master'){
+        console.log('deploy master..')
+        run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 
-        }
+        // }
 })
 
 
